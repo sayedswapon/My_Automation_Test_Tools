d1d407a82f8b71dcee00cf66d5ab63fbc1918db8
sayedswapon <swapon31@yahoo.com>
2014-03-30 21:47:22 -0700

    good boy

---
+++ phone hash.rb
@@ -0,0 +1,17 @@
+# Exmple of Hash:
+
+
+
+
+
+
+PhoneNumberHash = {   "Ravi" => "310-343-9796", 
+						"Kaiser" => "562-674-5823", 
+						"Sayed" => "310-853-9555"
+
+
+}
+
+
+
+						puts PhoneNumberHash["Ravi"]
